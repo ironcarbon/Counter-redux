c8d45bdb881083b2398eb3bdba3a24005d3be9f6
ipek <ipekcivicakan@gmail.com>
2018-12-06 09:52:28 -0800

    adding middleware, redux devtools

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/results';
 import { Provider } from 'react-redux';
@@ -14,7 +14,22 @@ const rootReducer = combineReducers({
     res: resultReducer
 })
 
-const store = createStore(rootReducer);
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('[Middleware] Dispatching', action);
+            const result = next(action);
+            console.log('[Middleware] next state', store.getState());
+            return result;
+        }
+    }
+};
+
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -4,3 +4,5 @@ export const ADD_FIVE = 'ADD_FIVE';
 export const SUBTRACT_FIVE = 'SUBTRACT_FIVE';
 export const STORE_RESULTS = 'STORE_RESULTS';
 export const DELETE_RESULTS = 'DELETE_RESULTS';
+
+
